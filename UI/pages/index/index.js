650e74a29e13ee2d44d884d2cf74aad0ef56ce0e
““Hazards” <799896781@qq.com>
2019-05-13 16:16:42 +0800

    第一次提交，部分界面不完整，后台暂时没有搭建

---
+++ UI/pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+Page({
+  data: {
+    PageCur: 'Schedule',
+  },
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
+  },
+  onShareAppMessage() {
+    return {
+      title: 'ColorUI-高颜值的小程序UI组件库',
+      imageUrl: '/images/share.jpg',
+      path: '/pages/index/index'
+    }
+  },
+})
